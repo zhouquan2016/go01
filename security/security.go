e707144d4bf763722c4f2e61bafdb696b3e9d3d8
周全 <code_master@sina.com>
2022-01-24 02:55:21 +0800

    golang实现rsa sha256 sign and verify(加签验签)

--- security/security.go
+++ security/security.go
@@ -1,12 +1,20 @@
 package security
 
 import (
+	"crypto"
 	"crypto/md5"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/hex"
 	"encoding/pem"
+	"errors"
+)
+
+const (
+	privateBlockType = "RSA PRIVATE KEY"
+	publicBlockType  = "RSA PUBLIC KEY"
 )
 
 // Md5 to encode string of src,
@@ -27,8 +35,52 @@ func GenRsaKeys(bits int) (priKey, pubKey string, err error) {
 		return "", "", err
 	}
 	priBytes := x509.MarshalPKCS1PrivateKey(key)
-	priKey = string(pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: priBytes}))
+	priKey = string(pem.EncodeToMemory(&pem.Block{Type: privateBlockType, Bytes: priBytes}))
 	pubBytes := x509.MarshalPKCS1PublicKey(&key.PublicKey)
-	pubKey = string(pem.EncodeToMemory(&pem.Block{Type: "RSA PUBLIC KEY", Bytes: pubBytes}))
+	pubKey = string(pem.EncodeToMemory(&pem.Block{Type: publicBlockType, Bytes: pubBytes}))
 	return priKey, pubKey, err
+}
+
+func SignWithRsa(privateKey string, src string) (string, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil || block.Type != privateBlockType {
+		return "", errors.New("failed to decode PEM block containing private key")
+	}
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	hash := crypto.SHA256.New()
+	hash.Write([]byte(src))
+	hashed := hash.Sum(nil)
+	signBytes, err := rsa.SignPKCS1v15(rand.Reader, key, crypto.SHA256, hashed)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(signBytes), nil
+}
+
+func VerifyWithRsa(signData string, srcData string, publicKey string) (bool, error) {
+
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil || block.Type != publicBlockType {
+		return false, errors.New("failed to decode PEM block containing private key")
+	}
+	key, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return false, err
+	}
+
+	bytes, err := base64.StdEncoding.DecodeString(signData)
+	if err != nil {
+		return false, err
+	}
+	hash := crypto.SHA256.New()
+	hash.Write([]byte(srcData))
+	hashed := hash.Sum(nil)
+	err = rsa.VerifyPKCS1v15(key, crypto.SHA256, hashed, bytes)
+	if err != nil {
+		return false, err
+	}
+	return true, err
 }
--- test/security_test.go
+++ test/security_test.go
@@ -84,3 +84,30 @@ func TestError(t *testing.T) {
 		fmt.Println(err)
 	}
 }
+
+func TestSign(t *testing.T) {
+	privateKey := "-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAtbkotioMGMkmtxhnJqa0EnYESNc4nm6CSYhEugp1+vOKKxRt\n2kDH6Mf8bwfcv3jdPgVWYBV8JEl0BcaIpIh4yM+secShcXjAVdaT4AAK7X2lNxwH\n8jzypedos2b7A/ajXofCAk1J2LTEAwQ5nnnVuZkQK1AiF8EKQJ63pkNTeKUxO/r4\nXXFCj9t/Z0KJzXXTzZKg3s0Ildwz5uNsZwVQ5doBHXfe8z6iPy/kQ39PnH6vjz3b\nG7w86QXcKlto7O0EGHBPc08wgfBcojYZuVZdZyi2LadqHN7MCBn7+yGJOldW9JFk\nA7QJCuwUe4OhYilDIkEz/st3f+3EyiT/Y3SEjwIDAQABAoIBAQCVCjUfokClm2RL\nSpJfGt4ZPf2pmApxtgTZNg/X3XiBx3gMeQbXt8W9XzfkADjGOOSLV2lOCJD2Wd/0\nVK4A84TMfbbhb6lRHU3xmLlTP/W2bhQwrKa/v8jq1G4EpXM9/uxiPUmbBbSJLUiN\niMuQ9C+btrOSMnijrskv0nyguhsdmEqDyl9GK6Om+8s+InbTYF2UEkfWmt3byMij\no/ZHeHOrNMAdtWgaO8tuP4lVgXSoSF1jGZ7iOpFJfqbDQFF6hzAH6qV9GMwMYp33\nuvn/JjJ/qv5hJyV8iuke/09CfZbElCGmUQQRxBH8V1oBaHFnelFXREaJAkRSd29g\n67l8MN8BAoGBAMHLrKBCbvpy275NmIX4xU9WcHp/fmry0oLbajW8VM/ki9VIGgJL\noSc7pXSifXsJ/QCsujv92h3AGVuFIe9bXOq3wVfzG3kssekzpOAY3HnsQem8Eg7P\nkzE1V/sbcUH7TtrgTlcWlWRyTp6azjnXkSAPmzRfy0ohiPt2tvZYPbLBAoGBAPAN\nfjITqQwA/xiQgbUo6La9QusXI09twhrVeskgEdmVycJkgoRrxqRki3AvnGHUEPfw\nZcwKQ2FVclrR3aKhHW/iPNf46xjg+yancKYfZOB4fAaxIX5z62+k7Z6s7qfEpOmm\nV9LuR/nDLbRmasSFXbZ0Yfovdjsew5ODoi+akBtPAoGAP+JIUbwUoXLjhWRG90L+\nqByyj28f2Vmak5CI/pXKz41jmzdde4w635gF/uDhxIGSaXqHGeeg01XUBhtpCGJx\nyt4wWjHFyLg3HczseQM5CugbAlYBDejXeM1A54IwX7PcVsLCkGrdbHNR/27AtMif\nCpaabzV06kcLxPlpbuO/0wECgYAh7OY+0YR8i8+Bezq5jZSF7u18KAL3gL2D6VO3\nBO9A3uhqrqW7bTffl84VBsWFFeFoCPN6CKFJKjhFGY/HIhn06/ZJV4ZyN6mG5vcG\noz0wdBajI3lmU5+cYaSrXilEUIg19SpIRyCo7aqR6j+AkpCR7pTCNN7ysABX4qyT\nKbbgBQKBgQCWSNbR4X1SSaFrTypxuHy3Gba30FckDEftE7UApqZBeM8zGPhvqb1P\nBDv9M9FCk54uLj4wvYTgyfb6p9LglFrs32lvEcejUHUSC4o9hksr+Exd9LylF11y\nu8ByLUudj4uwt+DkC/M4hiW4KGUM7PKx6jWVxJMbkfBMUnfIM7KwnA==\n-----END RSA PRIVATE KEY-----\n"
+	publicKey := "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAtbkotioMGMkmtxhnJqa0EnYESNc4nm6CSYhEugp1+vOKKxRt2kDH\n6Mf8bwfcv3jdPgVWYBV8JEl0BcaIpIh4yM+secShcXjAVdaT4AAK7X2lNxwH8jzy\npedos2b7A/ajXofCAk1J2LTEAwQ5nnnVuZkQK1AiF8EKQJ63pkNTeKUxO/r4XXFC\nj9t/Z0KJzXXTzZKg3s0Ildwz5uNsZwVQ5doBHXfe8z6iPy/kQ39PnH6vjz3bG7w8\n6QXcKlto7O0EGHBPc08wgfBcojYZuVZdZyi2LadqHN7MCBn7+yGJOldW9JFkA7QJ\nCuwUe4OhYilDIkEz/st3f+3EyiT/Y3SEjwIDAQAB\n-----END RSA PUBLIC KEY-----\n"
+	src := "xwww"
+	sign, err := security.SignWithRsa(privateKey, src)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(sign)
+
+	b, err := security.VerifyWithRsa(sign, src, publicKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(b)
+}
+
+//func TestVerify(t *testing.T) {
+//	publicKey := "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAtbkotioMGMkmtxhnJqa0EnYESNc4nm6CSYhEugp1+vOKKxRt2kDH\n6Mf8bwfcv3jdPgVWYBV8JEl0BcaIpIh4yM+secShcXjAVdaT4AAK7X2lNxwH8jzy\npedos2b7A/ajXofCAk1J2LTEAwQ5nnnVuZkQK1AiF8EKQJ63pkNTeKUxO/r4XXFC\nj9t/Z0KJzXXTzZKg3s0Ildwz5uNsZwVQ5doBHXfe8z6iPy/kQ39PnH6vjz3bG7w8\n6QXcKlto7O0EGHBPc08wgfBcojYZuVZdZyi2LadqHN7MCBn7+yGJOldW9JFkA7QJ\nCuwUe4OhYilDIkEz/st3f+3EyiT/Y3SEjwIDAQAB\n-----END RSA PUBLIC KEY-----\n"
+//	sign := "pm34wvzPORw3HnbSfj9ynLMEFIvYNxZrscktKnj+xGl6t44pnZPi8Z4F4Tem0XZ0/l3d2pWXh3UnwFObY1fs91jVrTotxNKyTc51GiwZfSOyn09kKZBaHNOZ/W3u04LyWUrX7+rrW3fpRqJEH1jVhpIKDNnb5K68lk7DTNsrqfqxS96jbduCOfgIZo2DGwvdAmXJUK4LwKNdSiwIZs1aYcXsFg4nKa0nOvZ55lED8eZzbzhmRV+Hr5M6wpTkR+AxYpiiWFXJjuikE5zGANkDH38qLeLkaLxqNdfLY5i/shs9LcsoKKR/Y/0KIXi/GYXrYHJPwhae/p0696R5aRD1tQ=="
+//	//b, err := security.VerifyWithRsa(sign, publicKey)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println(b)
+//}
